eb8af978c69f8b969e48ec7a423f420d494e030a
mochizuki-kazuki <mochizuki-kazuki@kayac.com>
2017-08-04 15:25:43 +0900

    menu template

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let mainWindow;
 app.on('ready', () => {
   machineId().then(id => {
     createWindow();
-    // createMenu();
+    createMenu();
     ipcMain.on('webview-ready', event => {
       // channel経由でレンダラープロセスに非同期に返信し、任意の引数を渡す
       // ipcMain.send()ではない点に注意
@@ -53,23 +53,31 @@ function createMenu() {
           click() {
             app.quit();
           }
-        },
-        {
-          role: 'separator'
-        },
-        {
-          role: 'resetzoom'
-        },
-        {
-          role: 'zoomin'
-        },
-        {
-          role: 'zoomout'
         }
       ]
-    }, {
-      label: 'Debug',
+    },
+    {
+      label: "Edit",
       submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" },
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Reload',
+          accelerator: 'CmdOrCtrl+R',
+          click(item, focusedWindow){
+            if(focusedWindow) focusedWindow.reload();
+          },
+        },
         {
           label: 'Toggle Developer Tools',
           accelerator: (process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I'),
@@ -78,7 +86,11 @@ function createMenu() {
               focusWindow.webContents.toggleDevTools();
             }
           }
-        }
+        },
+        { type: 'separator' },
+        { role: 'resetzoom' },
+        { role: 'zoomin' },
+        { role: 'zoomout' },
       ]
     }
   ];
